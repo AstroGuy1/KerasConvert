c63dee7a524ce8bf4a92ae8e51f5a97cf7d78d93
AstroGuy1 <97205625+AstroGuy1@users.noreply.github.com>
2024-07-31 16:33:38 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+# Load the Keras model
+keras_model_path = 'path/to/model.h5'
+model = tf.keras.models.load_model(keras_model_path)
+
+# Convert to TFLite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the TFLite model
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
